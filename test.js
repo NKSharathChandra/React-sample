de5b255e8c58417daa890521c9e8a7b56fda2839
NKSharathChandra <128453485+NKSharathChandra@users.noreply.github.com>
2023-03-30 16:25:00 +0530

    Add files via upload

---
+++ test.js
@@ -0,0 +1,16 @@
+function MyButton() {
+  return (
+    <button color='red'>
+      button
+    </button>
+  );
+}
+
+export default function MyApp() {
+  return (
+    <div>
+      <h1>Welcome to my app</h1>
+      <MyButton />
+    </div>
+  );
+}
